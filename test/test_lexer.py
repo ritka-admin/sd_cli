ed861358360a530d90720265daeb9aee5c633361
ritka_admin <margarita_lazareva30@list.ru>
2023-03-14 11:03:38 +0300

    modify workflow, add tests

---
+++ test/test_lexer.py
@@ -0,0 +1,12 @@
+
+from src.str_processing.lexer import lexer
+from src.classes.StringClass import InterpretString, PlainString
+
+
+def test_quotes():
+    raw_cmd = ("echo $HOME")
+    lexer_res = lexer(raw_cmd)
+    command, = lexer_res
+    assert len(lexer_res) == 1      # list of the one command tokens
+    assert isinstance(command[0], InterpretString)
+    assert isinstance(command[1], InterpretString)
